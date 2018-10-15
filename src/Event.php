93ef20fd1ace89bfb00388942f3129db0cbfea2a
古俊杰 <ilovintit@gmail.com>
2018-10-15 16:26:47 +0800

    增加事件与监听器相关逻辑定义

--- src/Event.php
+++ src/Event.php
@@ -4,7 +4,36 @@ namespace ZhiEq\Contracts;
 
 use Illuminate\Queue\SerializesModels;
 
-abstract class Event
+abstract class Event extends Attributes
 {
     use SerializesModels;
+
+    /**
+     * @return array
+     */
+
+    abstract protected function attributeMap();
+
+    /**
+     * @var array|mixed[]
+     */
+
+    public $initParams;
+
+    /**
+     * Event constructor.
+     * @param mixed ...$parameters
+     */
+
+    public function __construct(...$parameters)
+    {
+        foreach ($this->attributeMap() as $key => $attribute) {
+            if (isset($parameters[$key])) {
+                $this->setAttribute($attribute, $parameters[$key]);
+                unset($parameters[$key]);
+            }
+        }
+        $this->initParams = $parameters;
+        parent::__construct([]);
+    }
 }
